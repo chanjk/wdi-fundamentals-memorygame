41cbb5d9224f8c90a01eb8ef4ea7f7db31a605de
chanjk <chanjk@users.noreply.github.com>
2017-01-13 23:16:47 +1100

    Moved creation of card divs from HTML to JS, commented out IF statement in JS for now

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,11 +1,22 @@
 /* The four cards */
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-
+var cardOne = 'queen';
+var cardTwo = 'queen';
+var cardThree = 'king';
+var cardFour = 'king';
+/*
 if (cardTwo === cardFour) {
 	alert("You found a match!");
 } else {
 	alert("Sorry, try again.");
+}*/
+
+var board = document.getElementById('game-board');
+var createCards = function(numCards) {
+	for (i = 0; i < numCards; i++) {
+		newChild = document.createElement('div');
+		newChild.className = 'card';
+		board.appendChild(newChild);
+	}
 }
+
+createCards(4);
