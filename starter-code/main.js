c999cc168e1cbf0e57b5e0496b5ff425dfbd009e
chanjk <chanjk@users.noreply.github.com>
2017-01-15 22:14:42 +1100

    Updated game logic. Added card pictures. Game functional at a basic level

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,22 +1,66 @@
-/* The four cards */
-var cardOne = 'queen';
-var cardTwo = 'queen';
-var cardThree = 'king';
-var cardFour = 'king';
-/*
-if (cardTwo === cardFour) {
-	alert("You found a match!");
-} else {
-	alert("Sorry, try again.");
-}*/
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
 
-var board = document.getElementById('game-board');
-var createCards = function(numCards) {
-	for (i = 0; i < numCards; i++) {
-		newChild = document.createElement('div');
-		newChild.className = 'card';
-		board.appendChild(newChild);
+function isMatch(first, second) {
+	if (first.getAttribute('data-card') === second.getAttribute('data-card')) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+
+	resetBoard();
+};
+
+function isTwoCards() {
+	if (cardsInPlay.indexOf(this) < 0) {
+		cardsInPlay.push(this);
+		flip(this); 
+	}
+
+	setTimeout(function() {
+		if (cardsInPlay.length === 2) {
+			isMatch(cardsInPlay[0], cardsInPlay[1]);
+			cardsInPlay = [];
+		}
+	}, 0);
+}
+
+function flip(card) {
+	var image = { 'path' : '""',
+				  'alt' : '"Card not found"' };
+
+	switch (card.getAttribute('data-card')) {
+		case 'queen':
+			image = { 'path' : '"../images/queenSpades.png"',
+					  'alt' : '"Queen of Spades"' };
+			break;
+		case 'king':
+			image = { 'path' : '"../images/kingSpades.png"',
+					  'alt' : '"King of Spades"' };
+			break;
+	}
+
+	card.innerHTML = '<img src=' + image['path'] + ' alt=' + image['alt'] + '>';
+}
+
+function createBoard() {
+	var board = document.getElementById('game-board');
+
+	for (var i = 0; i < cards.length; i++) {
+		cardElement = document.createElement('div');
+		cardElement.className = 'card';
+		cardElement.setAttribute('data-card', cards[i]);
+		cardElement.addEventListener('click', isTwoCards);		
+		board.appendChild(cardElement);
+	}
+};
+
+function resetBoard() {
+	var cards = document.querySelectorAll('.card');
+
+	for (var i = 0; i < cards.length; i++) {
+		cards[i].innerHTML = '';
 	}
 }
 
-createCards(4);
+createBoard();
